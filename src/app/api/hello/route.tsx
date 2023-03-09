0b8aa333e1c72b9f3695f6dee3f0d2183c790dd3
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2023-03-09 11:57:51 +0530

    kHALI

--- src/app/api/hello/route.tsx
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  console.log("hello");
-  return NextResponse.json({ name: "Helllo" });
-}
